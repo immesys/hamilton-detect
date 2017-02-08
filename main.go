0e023822fbf6ae0aa24bbb7ad408d0b3601b3ca5
Michael Andersen <michael@steelcode.com>
2017-02-08 08:58:23 -0800

    First post

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	bw "gopkg.in/immesys/bw2bind.v5"
+)
+
+const TIMEOUT = 5 * time.Second
+
+func main() {
+	bwc := bw.ConnectOrExit("")
+	bwc.SetEntityFromEnvironOrExit()
+	subchan := bwc.SubscribeOrExit(&bw.SubscribeParams{
+		URI:       "amplab/brz/hbr003/s.hamilton/00126d0700000061/i.l7g/signal/raw",
+		AutoChain: true,
+	})
+	go func() {
+		for {
+			time.Sleep(100 * time.Millisecond)
+			checkLeft()
+		}
+	}()
+	for m := range subchan {
+		hamiltonHeard()
+	}
+}
+
+var isIn bool
+var stateMu sync.Mutex
+var lastHeard time.Time
+
+func hamiltonHeard() {
+	stateMu.Lock()
+	lastHeard = time.Now()
+	if !isIn {
+		isIn = true
+		stateMu.Unlock()
+		go hamiltonEntered()
+	}
+	stateMu.Unlock()
+}
+func checkLeft() {
+	stateMu.Lock()
+	if time.Now().Sub(lastHeard) > TIMEOUT {
+		isIn = false
+		stateMu.Unlock()
+		go hamiltonLeft()
+	}
+	stateMu.Unlock()
+}
+func hamiltonEntered() {
+	//JACK put your action here
+}
+func hamiltonLeft() {
+	//JACK put your action here
+}
